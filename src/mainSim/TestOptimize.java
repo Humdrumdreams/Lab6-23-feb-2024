c91a67440b2a20042f3661b1e4016b4a7a53e916
Botan Guzel <botanguzel@hotmail.com>
2024-02-28 09:58:30 +0100

    Update TestOptimize.java

--- src/mainSim/TestOptimize.java
+++ src/mainSim/TestOptimize.java
@@ -10,14 +10,14 @@ public class TestOptimize {
         SnabbköpTillstånd tillstånd = new SnabbköpTillstånd(antalKassor, maxAntalKunder, ankomstRate, frö, minKassaTid, maxKassaTid, minPlockTid, maxPlockTid, tidenSnabbköpetStänger);
         return tillstånd.getTotaltAntalMissadeKunder();
     }
-    public int metod2(double ankomstRate, long frö, double minKassaTid, double maxKassaTid, double minPlockTid, double maxPlockTid, double tidenSnabbköpetStänger) {
+    public int metod2(int maxAntalKunder, double ankomstRate, long frö, double minKassaTid, double maxKassaTid, double minPlockTid, double maxPlockTid, double tidenSnabbköpetStänger) {
         int low = 1;
         int high = Integer.MAX_VALUE; // Set a large upper bound for the number of cash registers
         int result = -1;
 
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            int missadeKunder = metod1(mid, Integer.MAX_VALUE, ankomstRate, frö, minKassaTid, maxKassaTid, minPlockTid, maxPlockTid, tidenSnabbköpetStänger);
+            int missadeKunder = metod1(mid, maxAntalKunder, ankomstRate, frö, minKassaTid, maxKassaTid, minPlockTid, maxPlockTid, tidenSnabbköpetStänger);
         
             if (missadeKunder == 0) {
                 // If no customers are missed, we try to minimize the number of cash registers further
@@ -33,7 +33,7 @@ public class TestOptimize {
     }
     public static void main(String[] args) {
         TestOptimize optimize = new TestOptimize();
-        System.out.println(optimize.metod2(1234L));
+        System.out.println(optimize.metod2(7, 3.0, 13L, 0.35, 0.6, 0.6, 0.9, 8.0));
         //Object[] hehe = optimize.metod3(1234L);
         //int maxKassor = (int) hehe[0];
         //long seed = (long) hehe[1];
